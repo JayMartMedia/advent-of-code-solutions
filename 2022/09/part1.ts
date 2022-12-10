9c80965303d20b217d76bc0fa2da62451754790f
JayMartMedia <Jay@JayMartMedia.com>
2022-12-09 23:08:41 -0500

    feat: complete 2022 day 9

--- 2022/09/part1.ts
+++ 2022/09/part1.ts
@@ -2,7 +2,7 @@
  * Run with `npx ts-node-dev --respawn ./part1.ts`
  */
 import setupUtils from '../../utils/ts/utils';
-import input from './einput';
+import input from './input';
 setupUtils();
 
 interface Position {
@@ -11,7 +11,7 @@ interface Position {
 }
 
 const lines = input.splitLines();
-const movements: {direction: string, amount: number}[] = lines.map(line => {
+const movements: { direction: string, amount: number }[] = lines.map(line => {
     const [direction, amount] = line.split(' ');
     return {
         direction,
@@ -19,44 +19,85 @@ const movements: {direction: string, amount: number}[] = lines.map(line => {
     }
 });
 
-const headPosition: Position = {x:0,y:0};
-const tailPosition: Position = {x:0,y:0};
+let headPosition: Position = { x: 0, y: 0 };
+let tailPosition: Position = { x: 0, y: 0 };
 
-// U L D R
+const tailPositions: string[] = ['0,0'];
 
-const directionLogic = {
-    U: (position: Position) => {
-        return {
-            x: position.x,
-            y: position.y + 1
-        }
-    },
-    R: (position: Position) => {
-        return {
-            x: position.x + 1,
-            y: position.y
-        }
-    },
-    D: (position: Position) => {
-        return {
-            x: position.x,
-            y: position.y - 1
-        }
-    },
-    L: (position: Position) => {
-        return {
-            x: position.x - 1,
-            y: position.y
-        }
+function moveUp(position: Position) {
+    return {
+        x: position.x,
+        y: position.y + 1
+    }
+}
+function moveRight(position: Position) {
+    return {
+        x: position.x + 1,
+        y: position.y
+    }
+}
+function moveDown(position: Position) {
+    return {
+        x: position.x,
+        y: position.y - 1
+    }
+}
+function moveLeft(position: Position) {
+    return {
+        x: position.x - 1,
+        y: position.y
     }
 }
 
+function tailTouchingHead(): boolean {
+    if (Math.abs(headPosition.x - tailPosition.x) <= 1 && Math.abs(headPosition.y - tailPosition.y) <= 1) {
+        return true
+    }
+    return false
+}
+
 movements.forEach(movement => {
-    for(let i = 0; i < movement.amount; i += 1){
+    for (let i = 0; i < movement.amount; i += 1) {
         // move head
-        const newHeadPosition = directionLogic[movement.direction](headPosition);
-        console.log(newHeadPosition)
+        let newHeadPosition: null | Position = null;
+        if (movement.direction === "U") newHeadPosition = moveUp(headPosition);
+        if (movement.direction === "R") newHeadPosition = moveRight(headPosition);
+        if (movement.direction === "D") newHeadPosition = moveDown(headPosition);
+        if (movement.direction === "L") newHeadPosition = moveLeft(headPosition);
+        // console.log(newHeadPosition)
+        if (newHeadPosition) {
+            headPosition = newHeadPosition;
+        }
+        let newTailPosition: null | Position = null;
+        if (!tailTouchingHead()) {
+            // move tail
+            if(headPosition.x === tailPosition.x){
+                // same row
+                newTailPosition = {x: tailPosition.x, y: (headPosition.y + tailPosition.y)/2};
+            }else if(headPosition.y === tailPosition.y){
+                // same col
+                newTailPosition = { x: (headPosition.x + tailPosition.x) / 2, y: tailPosition.y };
+            }else{
+                // diff row and col - move diag
+                if(Math.abs(headPosition.x - tailPosition.x) === 1){
+                    // move tail.x to head.x, move tail.y closer to head.y
+                    newTailPosition = {x: headPosition.x, y: (headPosition.y + tailPosition.y)/2};
+                }else{
+                    // move tail.x closer to head.x, move tail.y to head.y
+                    newTailPosition = { x: (headPosition.x + tailPosition.x) / 2, y: headPosition.y };
+                }
+            }
+        }
+
+        // add tail position
+        if (newTailPosition) {
+            tailPosition = newTailPosition;
+            tailPositions.push(`${tailPosition.x},${tailPosition.y}`);
+        }
     }
 });
 
-console.log(movements);
+// console.log(movements);
+console.log(tailPositions.uniq().length);
+
+// console.log(tailPositions.uniq())
--- 2022/09/part2.ts
+++ 2022/09/part2.ts
@@ -1,11 +1,98 @@
 /**
  * Run with `npx ts-node-dev --respawn ./part1.ts`
  */
- import setupUtils from '../../utils/ts/utils';
- import input from './einput';
- setupUtils();
- 
- const lines = input.splitLines();
- 
- console.log(input);
- console.log(lines);
+import setupUtils from '../../utils/ts/utils';
+import input from './input';
+setupUtils();
+
+interface Position {
+    x: number;
+    y: number;
+}
+
+const lines = input.splitLines();
+const movements: { direction: string, amount: number }[] = lines.map(line => {
+    const [direction, amount] = line.split(' ');
+    return {
+        direction,
+        amount: amount.toNumThrows()
+    }
+});
+
+let knotPositions: Position[] = [{ x: 0, y: 0 },{ x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }];
+
+const tailPositions: string[] = ['0,0'];
+
+function moveUp(position: Position) {
+    return {
+        x: position.x,
+        y: position.y + 1
+    }
+}
+function moveRight(position: Position) {
+    return {
+        x: position.x + 1,
+        y: position.y
+    }
+}
+function moveDown(position: Position) {
+    return {
+        x: position.x,
+        y: position.y - 1
+    }
+}
+function moveLeft(position: Position) {
+    return {
+        x: position.x - 1,
+        y: position.y
+    }
+}
+
+function followerTouchingLeader(leader: Position, follower: Position): boolean {
+    if (Math.abs(leader.x - follower.x) <= 1 && Math.abs(leader.y - follower.y) <= 1) {
+        return true
+    }
+    return false
+}
+
+movements.forEach(movement => {
+    for (let i = 0; i < movement.amount; i += 1) {
+        // move first knot
+        let newHeadPosition: null | Position = null;
+        if (movement.direction === "U") newHeadPosition = moveUp(knotPositions[0]);
+        if (movement.direction === "R") newHeadPosition = moveRight(knotPositions[0]);
+        if (movement.direction === "D") newHeadPosition = moveDown(knotPositions[0]);
+        if (movement.direction === "L") newHeadPosition = moveLeft(knotPositions[0]);
+        if (newHeadPosition) {
+            knotPositions[0] = newHeadPosition;
+        }
+        // move following knots
+        for(let j = 1; j < knotPositions.length; j+=1){
+            if (!followerTouchingLeader(knotPositions[j-1], knotPositions[j])) {
+                // move tail
+                if (knotPositions[j-1].x === knotPositions[j].x) {
+                    // same row
+                    knotPositions[j] = { x: knotPositions[j].x, y: (knotPositions[j-1].y + knotPositions[j].y) / 2 };
+                } else if (knotPositions[j-1].y === knotPositions[j].y) {
+                    // same col
+                    knotPositions[j] = { x: (knotPositions[j-1].x + knotPositions[j].x) / 2, y: knotPositions[j].y };
+                } else {
+                    // diff row and col - move diag
+                    if (Math.abs(knotPositions[j-1].x - knotPositions[j].x) === 1) {
+                        knotPositions[j] = { x: knotPositions[j-1].x, y: (knotPositions[j-1].y + knotPositions[j].y) / 2 };
+                    } else if (Math.abs(knotPositions[j-1].y-knotPositions[j].y) === 1){
+                        knotPositions[j] = { x: (knotPositions[j-1].x + knotPositions[j].x) / 2, y: knotPositions[j-1].y };
+                    } else {
+                        knotPositions[j] = { x: (knotPositions[j - 1].x + knotPositions[j].x) / 2, y: (knotPositions[j - 1].y + knotPositions[j].y) / 2 };
+                    }
+                }
+            }
+
+            if(j === knotPositions.length-1){
+                tailPositions.push(`${knotPositions[j].x},${knotPositions[j].y}`);
+            }
+        }
+    }
+});
+
+console.log(tailPositions.uniq().length);
