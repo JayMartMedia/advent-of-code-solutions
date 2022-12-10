cc5937882c2eceb0fc217acd3a8c49f214968f95
JayMartMedia <Jay@JayMartMedia.com>
2022-12-10 00:44:16 -0500

    feat: complete 2022 day 10

---
+++ 2022/10/part1.ts
@@ -0,0 +1,31 @@
+/**
+ * Run with `npx ts-node-dev --respawn ./part1.ts`
+ */
+import setupUtils from '../../utils/ts/utils';
+import input from './input';
+setupUtils();
+
+const lines = input.splitLines();
+const cycles: number[] = [1];
+
+let xreg = 1;
+
+lines.forEach(line => {
+    if(line === "noop") {
+        cycles.push(xreg);
+    }else{
+        const [_, value] = line.split(' ').toNums();
+        cycles.push(xreg);
+        cycles.push(xreg);
+        xreg += value;
+    }
+});
+
+console.log(
+    cycles[20] * 20 +
+    cycles[60] * 60 +
+    cycles[100] * 100 +
+    cycles[140] * 140 +
+    cycles[180] * 180 +
+    cycles[220] * 220
+);
---
+++ 2022/10/part2.ts
@@ -0,0 +1,40 @@
+/**
+ * Run with `npx ts-node-dev --respawn ./part1.ts`
+ */
+import setupUtils from '../../utils/ts/utils';
+import input from './input';
+setupUtils();
+
+const lines = input.splitLines();
+const cycles: string[] = [];
+
+let xreg = 1;
+
+function calc(num: number) {
+    return (cycles.length) % 40
+}
+
+lines.forEach(line => {
+    if (line === "noop") {
+        if(xreg-1 <= calc(cycles.length) && calc(cycles.length) <= xreg+1){
+            cycles.push('#');
+        }else{
+            cycles.push('.');
+        }
+    } else {
+        const [_, value] = line.split(' ').toNums();
+        if (xreg - 1 <= calc(cycles.length) && calc(cycles.length) <= xreg + 1) {
+            cycles.push('#');
+        } else {
+            cycles.push('.');
+        }
+        if (xreg - 1 <= calc(cycles.length) && calc(cycles.length) <= xreg + 1) {
+            cycles.push('#');
+        } else {
+            cycles.push('.');
+        }
+        xreg += value;
+    }
+});
+
+console.log(cycles.join('').divide(6));
