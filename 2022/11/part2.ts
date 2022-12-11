7936b276619c5cdacb9f2272a32353a075f1c647
JayMartMedia <Jay@JayMartMedia.com>
2022-12-11 16:00:54 -0500

    feat: complete 2022 day 11 part 2

--- 2022/11/part2.ts
+++ 2022/11/part2.ts
@@ -2,7 +2,7 @@
  * Run with `npx ts-node-dev --respawn ./part1.ts`
  */
 import setupUtils from '../../utils/ts/utils';
-import input from './einput';
+import input from './input';
 setupUtils();
 
 const monkeyInputs = input.split("\n\n");
@@ -16,9 +16,13 @@ interface Monkey {
 
 const monkeys: Monkey[] = []
 
+const clockFactors: number[] = [];
+
 monkeyInputs.forEach(monkeyInput => {
     const lines = monkeyInput.splitLines();
     const items: bigint[] = lines[1].split(": ")[1].split(',').map(num => BigInt(num));
+    const divisibleBy = lines[3].split('by ')[1].toNumThrows();
+    clockFactors.push(divisibleBy);
 
     function op(old: bigint) {
         const operation = lines[2].split('=')[1].trim();
@@ -55,17 +59,24 @@ monkeyInputs.forEach(monkeyInput => {
     });
 });
 
+let clock = BigInt(1);
+//console.log(clockFactors)
+for(let cf of clockFactors){
+    clock *= BigInt(cf);
+}
+//console.log(clock);
 // console.log(monkeys);
 
 // run rounds
 for (let i = 0; i < 10000; i += 1) {
-    if(i%100 === 0) console.log(i);
+    //if(i%100 === 0) console.log(i);
     monkeys.forEach(monkey => {
         // run op on all items
         // since monkey did not damage item
         monkey.items = monkey.items.map(item => {
             monkey.inspectionCount += 1;
-            return monkey.op(item);
+            const afterOp = monkey.op(item);
+            return afterOp % clock;
         });
         // test items and throw to other monkey
         while (monkey.items.length > 0) {
@@ -79,9 +90,9 @@ for (let i = 0; i < 10000; i += 1) {
     });
 }
 
-// console.log(monkeys);
+//console.log(monkeys);
 
 const monkeyInspectionCounts = monkeys.map(monkey => monkey.inspectionCount);
 const monkeyInspectionCountsSorted = monkeyInspectionCounts.sort((a, b) => b - a)
-console.log(monkeyInspectionCounts)
+//console.log(monkeyInspectionCounts)
 console.log(monkeyInspectionCountsSorted[0] * monkeyInspectionCountsSorted[1]);
